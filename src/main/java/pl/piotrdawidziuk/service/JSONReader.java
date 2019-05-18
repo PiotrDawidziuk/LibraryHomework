15b8e343dcccdd0696de8ae7eb49c1fe309277d4
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-05-18 19:01:03 +0200

    jsonarray

--- src/main/java/pl/piotrdawidziuk/service/JSONReader.java
+++ src/main/java/pl/piotrdawidziuk/service/JSONReader.java
@@ -1,29 +1,53 @@
 package pl.piotrdawidziuk.service;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JSONReader {
 	
+    @SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		
-	}
-	
-	  private static void parseBookObject(JSONObject book)
-	    {
-	        //Get employee object within list
-	        JSONObject bookObject = (JSONObject) book.get("items.");
-	         
-	        //Get employee first name
-	        String firstName = (String) bookObject.get("firstName");   
-	        System.out.println(firstName);
-	         
-	        //Get employee last name
-	        String lastName = (String) bookObject.get("lastName"); 
-	        System.out.println(lastName);
-	         
-	        //Get employee website name
-	        String website = (String) bookObject.get("website");   
-	        System.out.println(website);
-	    }
+    	  
+        JSONParser parser = new JSONParser();
+        
+        
+        //   https://stackoverflow.com/questions/18440098/org-json-simple-jsonarray-cannot-be-cast-to-org-json-simple-jsonobject
+        
+        // https://stackoverflow.com/questions/10926353/how-to-read-json-file-into-java-with-simple-json-library
+    	
+    	try {
+			JSONArray a = (JSONArray) parser.parse(new FileReader("books.json"));
+			 for (Object o : a)
+			  {
+			    JSONObject book = (JSONObject) o;
 
+			    String id = (String) book.get("id");
+			    System.out.println(id);
+
+			    String kind = (String) book.get("kind");
+			    System.out.println(kind);
+
+			    String etag = (String) book.get("etag");
+			    System.out.println(etag);
+
+			    //JSONArray cars = (JSONArray) person.get("ss");
+//
+//			    for (Object c : cars)
+//			    {
+//			      System.out.println(c+"");
+//			    }
+			  }
+			
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+    }
 }
