fd50b890c2cbc2116dbeb000fed881dfa632de62
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-05-18 18:24:43 +0200

    datasource

--- src/main/java/pl/piotrdawidziuk/service/JSONReader.java
+++ src/main/java/pl/piotrdawidziuk/service/JSONReader.java
@@ -1,11 +1,29 @@
 package pl.piotrdawidziuk.service;
 
+import org.json.simple.JSONObject;
+
 public class JSONReader {
 	
 	public static void main(String[] args) {
 		
 	}
 	
-	
+	  private static void parseBookObject(JSONObject book)
+	    {
+	        //Get employee object within list
+	        JSONObject bookObject = (JSONObject) book.get("items.");
+	         
+	        //Get employee first name
+	        String firstName = (String) bookObject.get("firstName");   
+	        System.out.println(firstName);
+	         
+	        //Get employee last name
+	        String lastName = (String) bookObject.get("lastName"); 
+	        System.out.println(lastName);
+	         
+	        //Get employee website name
+	        String website = (String) bookObject.get("website");   
+	        System.out.println(website);
+	    }
 
 }
