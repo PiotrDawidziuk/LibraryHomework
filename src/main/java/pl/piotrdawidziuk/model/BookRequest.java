d52f076945a1252781d2ab39907534fe9ec48bca
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-05-20 14:22:05 +0200

    book request

---
+++ src/main/java/pl/piotrdawidziuk/model/BookRequest.java
@@ -0,0 +1,15 @@
+package pl.piotrdawidziuk.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BookRequest {
+	
+	    private List<Item> items;
+
+}
